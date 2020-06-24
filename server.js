8f62c00dea907ba388cbe259cf2279513bd41838
thangy <unuhuynh@gmail.com>
2020-06-23 19:27:30 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-var express = require('express');
+var express = require('express');,
 
 var app = express();
 
+const PORT = 3000;
+
 app.use(express.static(__dirname));
 
-app.listen(3000);
+//app.listen(3000);
+
+app.listen(PORT,()=>{
+
+console.log(`App is running on port ${PORT}`);
+
+})
